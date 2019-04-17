3674f19eb8cf5784f77415c5445729d9d3736869
Michael Krog <michaelryankrog@gmail.com>
2019-04-16 18:28:09 -0600

    Testing Backend

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { apiKey, googleApi } from './keys'
-// import darkKey from './keys'
 
 import express from 'express'
 const app = express()
@@ -10,34 +9,52 @@ app.use(cors())
 
 app.get('/api/v1/weather/:lat/:long', async (request, response) => {
   const latitude = request.params.lat;
+  if(!latitude) return response.status(422).json('Latitude is missing')
   const longitude = request.params.long;
+  if(!longitude) return response.status(422).json('Longitude is missing')
   let returnInfo = []
   let weatherInfo = await retrieveWeather(latitude, longitude)
   let locationInfo = await retrieveLocation(latitude, longitude)
   returnInfo.push(weatherInfo)
   returnInfo.push(locationInfo)
+
   return response.json(returnInfo)
 })
 
 const retrieveWeather = async (latitude, longitude) => {
-  const response = await fetch(`https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}`);
-  const data = await response.json();
-  let locationInfo = await retrieveLocation(latitude, longitude)
-  return data;
+  try {
+    const response = await fetch(`https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}`);
+    if(!response.ok) { throw Error('Bad Request')}
+    const data = await response.json();
+    let locationInfo = await retrieveLocation(latitude, longitude)
+    return data;
+  } catch (error) {
+    return response.status(400).json(error.message)
+  }
+
 }
 
 const retrieveLocation = async (latitude, longitude) => {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleApi}`)
-  const data = await response.json();
-  const updatedAddress = await cleanAddress(data)
-  return updatedAddress
+  try {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleApi}`)
+    if(!response.ok) { throw Error('Bad Request')}
+    const data = await response.json();
+    const updatedAddress = await cleanAddress(data)
+    return updatedAddress
+  } catch(error) {
+    return response.status(400).json(error.message)
+  }
 }
 
 const cleanAddress = (data) => {
   let streetNumber = data.results[0].address_components[0].long_name;
+  if(!streetNumber) return response.status(422).json('streetNumber is missing')
   let route = data.results[0].address_components[1].long_name;
+  if(!route) return response.status(422).json('route is missing')
   let locality = data.results[0].address_components[3].long_name;
+  if(!locality) return response.status(422).json('locality is missing')
   let state = data.results[0].address_components[5].long_name;
+  if(!state) return response.status(422).json('state is missing')
   let addressInfo = {
     city: locality,
     state: state,
